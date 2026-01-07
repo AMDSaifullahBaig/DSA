ba875796f139e00937bab950256c0a9f9f58d813
A.MD Saifullah Baig <mdsaifu2007@gmail.com>
2026-01-07 18:15:58 +0530

    Solved 0392 is subsequence using Fast and Slow Pointers

---
+++ Array_and_Hashmap/Fast_and_Slow_Pointers/0392_Is_Subsequence/Fast_and_Slow_Pointers/Fast_and_Slow_Pointers.py
@@ -0,0 +1,35 @@
+"""
+--------------------------------------------------------------------------------
+File: Two_Pointers.py
+Problem: 0392. Is Subsequence
+Link: https://leetcode.com/problems/is-subsequence/
+
+Author: MD Saifullah Baig.A
+Date: 07.01.2026
+
+Description:
+    This approach uses two pointers to traverse both strings 's' and 't'.
+    - Pointer 'i' tracks our progress in 's' (the subsequence we want to find).
+    - Pointer 'j' iterates through 't' (the source string).
+    
+    If characters match (s[i] == t[j]), we move 'i' forward.
+    We always move 'j' forward.
+    
+    If 'i' reaches the end of 's', it means we found all characters in order.
+    
+    Time Complexity: O(T) - We iterate through 't' once.
+    Space Complexity: O(1) - Constant extra space.
+--------------------------------------------------------------------------------
+"""
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        i, j = 0, 0
+        if not s:
+            return True
+        while i<len(t):
+            if i<len(s) and s[i]==t[j]:
+                i+=1
+            if i==len(s):
+                return True
+            j+=1
+        return i==len(s)
