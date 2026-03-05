973ef9beb62a6c859c7e2088ac2a5ac3da5cb4fc
A.MD Saifullah Baig <mdsaifu2007@gmail.com>
2026-03-05 18:56:53 +0530

    Added Submission Results to 0876. Middle of the Linked List

--- Linked_List/Fast_And_Slow_Pointers/0876_Middle_Of_The_Linked_List.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        fast=head
-        while fast and fast.next:
-        	fast=fast.next.next
-        	head=head.next
-        return head
