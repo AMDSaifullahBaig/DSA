d89ce198e45252de264dbb5a8310cde269e11975
A.MD Saifullah Baig <mdsaifu2007@gmail.com>
2026-03-16 18:54:25 +0530

    Solved 0215_Kth_Largest_Element_In_An_Array

---
+++ Heap/0215_Kth_Largest_Element_In_An_Array/Min_Heap.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heap=nums[:k]
+        heapq.heapify(heap)
+        for i in nums[k:]:
+            if i>heap[0]:
+                heapq.heapreplace(heap,i)
+        return heap[0]
