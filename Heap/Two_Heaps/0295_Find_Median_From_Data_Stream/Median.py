525e0922a5ebe719591f57814b5307a01bf35944
A.MD Saifullah Baig <mdsaifu2007@gmail.com>
2026-03-21 18:08:15 +0530

    Solved 0295. Find Median from Data Stream

---
+++ Heap/Two_Heaps/0295_Find_Median_From_Data_Stream/Median.py
@@ -0,0 +1,14 @@
+class MedianFinder:
+    def __init__(self):
+        self.small=[]
+        self.large=[]
+    def addNum(self, num: int) -> None:
+        heappush(self.small,-num)
+        heappush(self.large,-heappop(self.small))
+        if len(self.large)>len(self.small):
+        	heappush(self.small,-heappop(self.large))
+    def findMedian(self) -> float:
+    	if len(self.small)>len(self.large):
+    		return -self.small[0]
+    	else:
+    		return (-self.small[0]+self.large[0])/2
