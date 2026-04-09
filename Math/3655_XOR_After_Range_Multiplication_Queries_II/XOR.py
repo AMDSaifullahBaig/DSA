190c2c454fd98f308a89b1febff4b45e5016be1d
A.MD Saifullah Baig <mdsaifu2007@gmail.com>
2026-04-09 22:40:12 +0530

    Added Submission Results for 3655. XOR After Range Multiplication Queries II

--- Math/3655_XOR_After_Range_Multiplication_Queries_II/XOR.py
+++ Math/3655_XOR_After_Range_Multiplication_Queries_II/XOR.py
@@ -19,7 +19,7 @@ class Solution:
                 diff[l] = (diff[l] * v) % MOD
                 last_idx = l + ((r - l) // k + 1) * k
                 inv_v = pow(v, MOD - 2, MOD)
-                diff[last_idx] = (diff[last_idx] * inv_v) % swee
+                diff[last_idx] = (diff[last_idx] * inv_v) % MOD
             for i in range(n):
                 if i >= k:
                     diff[i] = (diff[i] * diff[i-k]) % MOD
