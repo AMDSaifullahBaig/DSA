dba7094a93faedd5c20dc8d519a16b3a6454e7bb
AMDSaifullahBaig <mdsaifu2007@gmail.com>
2026-06-27 18:50:12 +0530

    Solved 3020. Find the Maximum Number of Elements in Subset using counter

---
+++ Array_and_Hashmap/Hashing/3020_Find_The_Maximum_Number_of_Elements_In_Subset/Counter.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maximumLength(self, nums: List[int]) -> int:
+        maximum=0
+        hash=Counter(nums)
+        for i in hash:
+            if i==1:
+                total=hash[i]D if hash[i]&1 else hash[i]-1
+            else:
+                total=0
+                curr=i
+                while curr**2 in hash and hash[curr]>=2:
+                    total+=2
+                    curr=curr**2
+                total+=1
+            maximum=max(maximum,total)
+        return maximum
